9fcd155e9b2eb7895b0ee884bad33d1d2650ce07
kalaharileeu <cfdelange@gmail.com>
2016-02-12 11:32:29 +1300

    tweeked paramters

---
+++ PyLauncher2Tests/SerialIOportTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace PyLauncher.Tests
+{
+    [TestClass()]
+    public class SerialIOportTests
+    {
+        SerialIOport IO = new SerialIOport();
+         
+        [TestMethod()]
+        public void GetserialportnamesTest()
+        {
+            //Arrange
+            SerialIOport IO = new SerialIOport();
+            IO.Getserialportnames();
+            //IO.CheckPortName("COM12");
+            //Act
+            Debug.WriteLine(IO.Getportstring());
+            //Assert
+            Assert.AreEqual(IO.CheckPortName(""), false);
+
+        }
+
+        //[TestMethod()]
+        //public void CheckPortNameTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
---
+++ PyLauncher2Tests/TestrunnerNOstopTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PyLauncher.Tests
+{
+    [TestClass()]
+    public class TestrunnerNOstopTests
+    {
+        [TestMethod()]
+        public void LoadTest()
+        {
+            //arrange
+            TestrunnerNOstop TestrunnerNOstop = new TestrunnerNOstop();
+            Parameter tests = new Parameter();
+            tests.tests.Add(new Parameter.Test());
+            tests.tests[0].workingdirectory = "c:\\workspace\\teststuff";
+            tests.tests[0].filename = "python.exe";
+            tests.tests[0].arguments = "C:\\workspace\\teststuff\\testtime.py";
+            //Act
+            TestrunnerNOstop.Load(tests.tests[0]);
+            //Assert
+            /*There should be a cmdline window popping up, with some 
+            with some test scrip running*/
+        }
+
+        [TestMethod()]
+        public void RuntestTest()
+        {
+            //arrange
+           // Parameter tests;
+            TestrunnerNOstop TestrunnerNOstop = new TestrunnerNOstop();
+            string value = "C:\\workspace\\teststuff\\testtime.py";
+            //Act Assert
+            TestrunnerNOstop.Runtest(value, "python.exe", "c://workspace//teststuff");
+            //Assert
+            /*There should be a cmdline window popping up, with some
+            with some test scrip running*/
+        }
+    }
+}
